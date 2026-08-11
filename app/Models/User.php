1e7e56838880c7bf094d5acd013e284cdced956a
Himanshu Parmar <himanshu.parmar@rku.ac.in>
2026-08-12 00:21:47 +0530

    403 bug fixing

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,10 +9,11 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
@@ -28,5 +29,16 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // Option 1: Allow everyone (Not recommended for actual production security, but good for testing)
+        // return true; 
+
+        // Option 2: Restrict by email domain
+        // return str_ends_with($this->email, '@yourcompany.com') && $this->hasVerifiedEmail();
+
+        // Option 3: Restrict by a boolean column in your database
+        return $this->is_admin === 1;
     }
 }
